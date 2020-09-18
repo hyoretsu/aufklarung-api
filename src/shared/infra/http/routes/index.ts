416615f91b09c618db4179a6e39c6a6b003a5941
Aran Leite <hyoretsu@gmail.com>
2020-09-18 13:44:24 -0300

    Inital cleaning/update
    General template editing to fit current app's needs

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,9 +1,5 @@
 import { Router } from 'express';
 
-import exampleRouter from '@modules/example/infra/http/routes/example.routes
-
 const routes = Router();
-
-routes.use('/', exampleRouter);
 
 export default routes;
