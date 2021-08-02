3d0d4e66db3ec72ed15fe02622044340b3e54788
Aran Leite <hyoretsu@gmail.com>
2021-08-01 22:28:53 -0300

    Fixed users repository not being injected

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -4,5 +4,11 @@ import './providers';
 
 import IssuesRepository from '@modules/issues/infra/typeorm/repositories/IssuesRepository';
 import IIssuesRepository from '@modules/issues/repositories/IIssuesRepository';
+import NewsRepository from '@modules/news/infra/typeorm/repositories/NewsRepository';
+import INewsRepository from '@modules/news/repositories/INewsRepository';
+import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 
 container.registerSingleton<IIssuesRepository>('IssuesRepository', IssuesRepository);
+container.registerSingleton<INewsRepository>('NewsRepository', NewsRepository);
+container.registerSingleton<IUsersRepository>('UsersRepository', UsersRepository);
