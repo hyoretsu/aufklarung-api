699251435a00ead7262d9654be39c55893ab19d5
Aran Leite <hyoretsu@gmail.com>
2021-08-02 16:17:16 -0300

    Fixed user creation not working
    There was a column with an incompatible name, user's entity was set up wrong, findByEmail() had a slight error in its implementation and HashProvider wasn't being injected

--- src/modules/users/infra/typeorm/entities/User.ts
+++ src/modules/users/infra/typeorm/entities/User.ts
@@ -1,6 +1,6 @@
 import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
 
-@Entity()
+@Entity('users')
 export default class User {
  @PrimaryGeneratedColumn('uuid')
  id: string;
